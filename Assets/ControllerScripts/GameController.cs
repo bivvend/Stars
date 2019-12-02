3b174ed8d97c0913cc4dd47689fabc237a021fa8
bivvend <simon.j.henley@gmail.com>
2019-12-02 23:19:42 +0000

    asteroids!

--- Assets/ControllerScripts/GameController.cs
+++ Assets/ControllerScripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -10,17 +11,26 @@ public class GameController : MonoBehaviour
     public int NumShips = 1;
     public List<string> PlanetNames = new List<string>();
 
-    public float EarthDist;
-    public float MoonEarthDistance;    
-    public float MarsDistance;
-    public float PlutoDistance;
     public float BoardSizeX;
     public float BoardSizeY;
     public float PlanetZ;
 
+    public float MinAsteroidDistance;
+    public float MaxAsteroidDistance;
+
     public GameObject Earth;
     public GameObject Mars;
     public GameObject Pluto;
+
+    public int NumAsteroids;
+
+    public List<GameObject> AsteroidBelt;
+    public GameObject Asteroid; //Asteroid prefab
+    public GameObject Asteroid1; //Asteroid prefab
+    public GameObject Asteroid2; //Asteroid prefab
+    public GameObject Asteroid3; //Asteroid prefab
+    public GameObject Asteroid4; //Asteroid prefab
+    public GameObject Asteroid5; //Asteroid prefab
 
     void Awake()
     {
@@ -44,22 +54,69 @@ public class GameController : MonoBehaviour
 
     void SetupBoard()
     {
+        //Define setup
         BoardSizeX = 4000.0f;
         BoardSizeY = 4000.0f;
 
-        EarthDist = 0.107f;
-        MarsDistance = 0.165f;
-        PlutoDistance = 0.95f;
+        MinAsteroidDistance = 600.0f;
+        MaxAsteroidDistance = 800.0f;
 
+        NumAsteroids = 500;
         PlanetZ = 1.0f;
 
+        //Store object references
         Earth = GameObject.Find("Earth");
         Mars = GameObject.Find("Mars");
         Pluto = GameObject.Find("Pluto");
 
-        Earth.transform.position = new Vector3(EarthDist * BoardSizeX / 2.0f, 0, PlanetZ);
-        Mars.transform.position = new Vector3(0, MarsDistance * BoardSizeY / 2.0f, PlanetZ);
-        Pluto.transform.position = new Vector3(PlutoDistance * BoardSizeX / 2.0f, 0, PlanetZ);
+        //GENERATE OBJECTS
+        //Asteroids
+        float r = 0.0f;
+        float angle = 0.0f;
+        float scale = 0.0f;
+        foreach(int i in Enumerable.Range(0,NumAsteroids))
+        {
+            r = Random.Range(MinAsteroidDistance, MaxAsteroidDistance);
+            angle = Random.Range(0, 360);
+            scale = Random.Range(2.0f, 5.0f);
+            int type = (int)(Random.Range(0, 6.0f));
+            GameObject newAsteroid;
+            switch(type)
+            {
+                case 0:
+                    newAsteroid = Instantiate(Asteroid);
+                    break;
+                case 1:
+                    newAsteroid = Instantiate(Asteroid1);
+                    break;
+                case 2:
+                    newAsteroid = Instantiate(Asteroid2);
+                    break;
+                case 3:
+                    newAsteroid = Instantiate(Asteroid3);
+                    break;
+                case 4:
+                    newAsteroid = Instantiate(Asteroid4);
+                    break;
+                case 5:
+                    newAsteroid = Instantiate(Asteroid5);
+                    break;
+                default:
+                    newAsteroid = Instantiate(Asteroid);
+                    break;
+
+
+            }
+
+            
+            newAsteroid.transform.localScale = new Vector3(scale, scale, 1.0f);
+            ObjectRotate rotateScript = newAsteroid.GetComponent<ObjectRotate>();
+            rotateScript.OrbitRadius = r;
+            rotateScript.OrbitalPeriod = 10000.0f + r * 5.0f;
+            rotateScript.AngleDegs = angle;
+            rotateScript.SpinRate = Random.Range(-0.5f, 0.5f);
+            AsteroidBelt.Add(newAsteroid);
+        }
     }
 
     void OnGUI()
